3c94bf701dec7a71f032759d3ff7218974f69f17
Caldo-pixel <200602048@student.georgianc.on.ca>
2024-11-29 19:43:51 -0500

    started adding player movement and rotation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float currentSpeed;
+    float maxSpeed;
+
+    float movementRate = 0.1f;
+    public float dragCoefficient = 0.8f;
+
+    //Vector3 dragForce;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentSpeed = 0.0f;
+
+        maxSpeed = currentSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementInput();
+
+        MovementProcess();
+
+        Rotation();
+    }
+
+    void MovementInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            maxSpeed = 1.0f;
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            maxSpeed = 0.0f;
+        }
+    }
+
+    void MovementProcess()
+    {
+        currentSpeed = Mathf.MoveTowards(currentSpeed, maxSpeed, movementRate * Time.deltaTime);
+        Vector3 change = transform.up * currentSpeed;
+        transform.position += change;
+
+        //Vector3 dragForce = -change * dragCoefficient;
+        //transform.position += dragForce;
+    }
+
+    void Rotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f, 0f, 150f * Time.deltaTime, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, 0f, -150f * Time.deltaTime, Space.Self);
+        }
+    }
+}
